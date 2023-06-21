df5995733f1b466831d582f41d6ef62fa8ac5ddf
marc [itb] crusellas serra <marc.crusellas.7e4@itb.cat>
2023-06-21 19:11:22 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    cout << "aaa" << "\n";
+    
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    cout << "aaa" << "\n";
+    
+    return 0;
+}
